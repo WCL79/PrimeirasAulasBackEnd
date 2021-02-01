1bda5ed5c9b181599beee01c688e0229e291d256
Weslley Lima <weslley.lima@zup.com.br>
2021-02-01 16:27:49 -0300

    Complemento da aula 01/02/21

---
+++ src/com/catalisa/aula01022021/ListaDeAnimais.java
@@ -0,0 +1,36 @@
+package com.catalisa.aula01022021;
+
+import java.util.*;
+
+public class ListaDeAnimais {
+
+    public static void main(String[] args) {
+        // Programa que cadastra cada animal de cada zoologico de cada cidade.
+
+        Map<String, List<String>> dicionarioAnimais = new HashMap<>();
+
+        boolean continuar = true;
+        while (continuar){
+            System.out.println("Digite: \n1 para cadastrar");
+            int resposta = new Scanner(System.in).nextInt();
+
+            if(resposta == 1){
+                System.out.println("Qual zoologico vc vai cadastrar? ");
+                String zoologico = new Scanner(System.in).nextLine();
+                List<String> animais = new ArrayList<>();
+                String retorno = "";
+                do{
+                    System.out.println("Digite o nome do animal para cadastrar ou sair para finalizar");
+                    retorno = new Scanner(System.in).nextLine();
+                    if(!retorno.equalsIgnoreCase("sair")){
+                        animais.add(retorno);
+                    }
+                }while (!retorno.equalsIgnoreCase("sair"));
+
+                dicionarioAnimais.put(zoologico, animais);
+            }
+
+        }
+
+    }
+}
---
+++ src/com/catalisa/aula01022021/ListaDeAnimais2.java
@@ -0,0 +1,44 @@
+package com.catalisa.aula01022021;
+
+import java.util.*;
+
+public class ListaDeAnimais2 {
+
+    public static Scanner criarScanner(){
+        return new Scanner(System.in);
+    }
+
+    public static List<String> criarListaDeAnimais(){
+        List<String> animais = new ArrayList<>();
+        String retorno = "";
+        do{
+            System.out.println("Digite o nome do animal para cadastrar ou sair para finalizar");
+            retorno = criarScanner().nextLine();
+            if(!retorno.equalsIgnoreCase("sair")){
+                animais.add(retorno);
+            }
+        }while (!retorno.equalsIgnoreCase("sair"));
+        return animais;
+    }
+
+    public static void main(String[] args) {
+        // Programa que cadastra cada animal de cada zoologico de cada cidade.
+
+        Map<String, List<String>> dicionarioAnimais = new HashMap<>();
+
+        boolean continuar = true;
+        while (continuar){
+            System.out.println("Digite: \n1 para cadastrar");
+            int resposta = criarScanner().nextInt();
+
+            if(resposta == 1){
+                System.out.println("Qual zoologico vc vai cadastrar? ");
+                String zoologico = criarScanner().nextLine();
+                List<String> animais = criarListaDeAnimais();
+                dicionarioAnimais.put(zoologico, animais);
+            }
+
+        }
+
+    }
+}
